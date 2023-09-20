29c85f1e9e533fbcdbf42680bae09506b17d6b05
kashi <kashisharmaujjn@gmail.com>
2023-09-20 21:38:05 +0530

    Registration Portal

--- src/route/route.js
+++ src/route/route.js
@@ -1,20 +1,8 @@
 const express = require('express')
 const router = express.Router()
-// const {validateIP} = require('../middleware/middleware')
-// const {router} = require('../models/ipUser')
 const Register =require('../models/user')
 const registerUser = require('../controllers/userOtp')
 const verifyUser= require('../controllers/verifyotp')
-
-//ip route
-// router.get('/ip', validateIP)
-// router.get('/ip',validateIP, call);
-// router.get('/secure-route', (req, res) => {
-//     const { country, city } = req; // Access geolocation data
-  
-    // Implement your route logic here
-//     res.json({ country, city });
-//   });
 
 router.get('/' , (req,res)=>{
     res.send("router method")
